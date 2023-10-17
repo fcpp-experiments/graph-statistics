66d48155bd481758d37fe448be884fc97624ffb7
Gianluca Torta <giatorta@gmail.com>
2023-10-17 20:29:47 +0200

    HBApp for debug

---
+++ java/for-debugging/HBApp.java
@@ -0,0 +1,81 @@
+package it.unimi.dsi.webgraph;
+
+import it.unimi.dsi.webgraph.*;
+import it.unimi.dsi.webgraph.algo.HyperBall;
+import it.unimi.dsi.webgraph.algo.NeighbourhoodFunction;
+
+import it.unimi.dsi.logging.ProgressLogger;
+
+import java.io.FileWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class HBApp {
+    public static final String syntax = "it.unimi.dsi.webgraph.HBApp {stats|conv|enc} <file>";
+
+    public static final boolean DEPTH_LIMIT = false;
+
+    public static void main(String[] args) throws IOException {
+        if (args.length < 2) {
+            System.out.println(syntax);
+            return;
+        }
+        String fname = args[1];
+        if (args[0].equals("conv")) {
+            BVGraph g = BVGraph.load(fname, 0);
+            g.store(g,fname);
+            g = BVGraph.load(fname, 2);
+            ASCIIGraph.store(g, fname);
+        } else if (args[0].equals("enc")) {
+            try {
+                BVGraph.main(new String[]{"-g", "ASCIIGraph", fname, fname});
+            } catch (Exception e) {
+                System.out.println("Encoding failed with error " + e.toString());
+            }
+        } else if (args[0].equals("stats")) {
+            BVGraph g = BVGraph.load(fname, 0);
+            g.store(g,fname);
+            g = BVGraph.load(fname, 2);
+            System.out.println("number of nodes: " + g.numNodes());
+
+            HyperBall hb = new HyperBall(g, g, 7, new ProgressLogger(), 1, 0, 0, false, true, true, null, 0);
+            hb.init();
+
+            hb.iterate();
+
+            int depth=1;
+            while (hb.modified() != 0 || (DEPTH_LIMIT && depth<10)) {
+                hb.iterate();
+                depth++;
+            }
+            FileWriter out = new FileWriter(fname + ".out");
+            for (int i=0; i<g.numNodes(); i++)
+                out.write(hb.sumOfInverseDistances[i] + " " + 1.0/hb.sumOfDistances[i] + "\n");
+            out.close();
+            double sum=0;
+            double min=hb.sumOfInverseDistances[0];
+            double max=hb.sumOfInverseDistances[0];
+            double sumC=0;
+            double minC=hb.sumOfDistances[0];
+            double maxC=hb.sumOfDistances[0];
+            for (int i=0; i<g.numNodes(); i++) {
+                sum += hb.sumOfInverseDistances[i];
+                if (hb.sumOfInverseDistances[i] < min) min = hb.sumOfInverseDistances[i];
+                if (hb.sumOfInverseDistances[i] > max) max = hb.sumOfInverseDistances[i];
+                sumC += hb.sumOfDistances[i];
+                if (hb.sumOfDistances[i] < minC) minC = hb.sumOfDistances[i];
+                if (hb.sumOfDistances[i] > maxC) maxC = hb.sumOfDistances[i];
+            }
+            System.out.println("min, mean, max Harmonic Centrality:");
+            System.out.println(min + ", " + sum/g.numNodes() + ", " + max);
+            System.out.println("min, mean, max Closeness Centrality:");
+            System.out.println(1.0/minC + ", " + g.numNodes()/sumC + ", " + 1.0/maxC);
+            System.exit(0);
+        } else {
+            System.out.println(syntax);
+        }
+    }
+}
